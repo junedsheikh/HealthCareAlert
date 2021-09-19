69783a7fd57e78f10ad194523e3c1aa1f0acf481
junedsheikh <90326044+junedsheikh@users.noreply.github.com>
2021-09-19 14:55:33 +0530

    Update Health Care Alert.py
    # Health Care Alert for a programmer who don't take care of health on office time 9AM to 5PM
    # Drinking water every 20 minutes = 1200sec
    # Eye exercise every 30 minutes = 1800sec
    # Body exercise every 45 minutes = 2700sec
    # We can use pygame module to play music

--- Health Care Alert.py
+++ Health Care Alert.py
@@ -1,8 +1,3 @@
-# Health Care Alert for a programmer who don't take care of hea9lth on office time 9AM to 5PM
-# Drinking water every 20 minutes = 1200sec
-# Eye exercise every 30 minutes = 1800sec
-# Body exercise every 45 minutes = 2700sec
-# We can use pygame module to play song.mp3
 import time
 import winsound
 
